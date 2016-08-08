ce215e35e5fc5b5064fe20e72207459aa2de5221
J/A <archer884@gmail.com>
2016-08-08 14:11:07 -0500

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,8 @@
 #[macro_use] mod try_opt;
 
 mod intersection;
-mod difference;
-mod distinct;
 
 pub use intersection::{
     AscendingIntersection,
     DescendingIntersection,
-};
-
-pub use difference::{
-    AscendingDifference,
-    DescendingDifference,
-};
-
-pub use distinct::{
-    AscendingDistinct,
-    DescendingDistinct,
 };
